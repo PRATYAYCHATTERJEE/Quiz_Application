b538264e1cbb91563098ce4898aa5f8e762ee3f6
PRATYAY CHATTERJEE <pratyay.ch2006@gmail.com>
2026-07-19 23:25:53 +0530

    commit

---
+++ Fronted/quiz3.js
@@ -0,0 +1,16 @@
+/* ==========================================================
+   QUIZVERSE QUIZ.JS
+========================================================== */
+
+/* ==========================================================
+   VARIABLES
+========================================================== */
+
+let questions = [];
+let currentQuestion = 0;
+let selectedAnswers = [];
+let reviewQuestions = [];
+let skippedQuestions = [];
+let quizFinished = false;
+let totalTime = 20 * 60;
+let timerInterval = null;
