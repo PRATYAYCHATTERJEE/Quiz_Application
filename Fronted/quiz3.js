b85cb713ca8ed85ac5c86c13f487e6c75a04c3a6
PRATYAY CHATTERJEE <pratyay.ch2006@gmail.com>
2026-07-19 23:26:53 +0530

    commit

--- Fronted/quiz3.js
+++ Fronted/quiz3.js
@@ -14,3 +14,22 @@ let skippedQuestions = [];
 let quizFinished = false;
 let totalTime = 20 * 60;
 let timerInterval = null;
+
+
+/* ==========================================================
+   DOM ELEMENTS
+========================================================== */
+
+const questionNumber = document.getElementById("questionNo");
+const questionText = document.getElementById("questionText");
+
+const optionCards = document.querySelectorAll(".option-card");
+
+const currentQuestionText = document.getElementById("currentQuestion");
+const totalQuestionText = document.getElementById("totalQuestions");
+
+const progressFill = document.querySelector(".progress-fill");
+
+const timer = document.getElementById("quizTimer");
+
+const navigatorButtons = document.querySelectorAll(".nav-btn");
